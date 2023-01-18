6c9d629fa156bfed3ba075c4a44e6e44ea500998
D4nielJ <d4niel.djm@gmail.com>
2023-01-17 21:33:48 -0500

    project init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { HelloWorld } from "./HelloWorld";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/HelloWorld.tsx
@@ -0,0 +1,5 @@
+export function HelloWorld() {
+  return (
+    <div style="background-color: #2c4f7c; color: #FFF;">Hello world!</div>
+  );
+}
