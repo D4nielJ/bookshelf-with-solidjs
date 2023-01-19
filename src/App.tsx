4dd2fd208248905ac4e74b67c83d56711e607d81
D4nielJ <d4niel.djm@gmail.com>
2023-01-18 21:48:59 -0500

    Adding conditional rendering to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createSignal } from "solid-js";
+import { createSignal, Show } from "solid-js";
 import { Book } from "./App.types";
 import { AddBook } from "./components/AddBook/AddBook";
 import { BookList } from "./components/BookList/BookList";
@@ -7,20 +7,35 @@ export interface BookshelfProps {
   name: string;
 }
 
-const initialBooks: Book[] = [
-  { title: "Code Complete", author: "Steve McConnell" },
-  { title: "The Hobbit", author: "J.R.R. Tolkien" },
-  { title: "Living a Feminist Life", author: "Sarah Ahmed" },
-];
+const initialBooks: Book[] = [];
 
 function Bookshelf(props: BookshelfProps) {
   const [books, setBooks] = createSignal(initialBooks);
+  const [showForm, setShowForm] = createSignal(false);
+  const toggleForm = () => setShowForm(!showForm());
 
   return (
     <div>
       <h1>{props.name}'s bookshelf</h1>
-      <BookList books={books()} />
-      <AddBook setBooks={setBooks} />
+
+      <Show
+        when={books().length > 0}
+        fallback={
+          <>
+            <p>You have no books in your bookshelf.</p>
+          </>
+        }
+      >
+        <BookList books={books()} />
+      </Show>
+
+      <Show
+        when={showForm()}
+        fallback={<button onClick={toggleForm}>Add a book</button>}
+      >
+        <AddBook setBooks={setBooks} />
+        <button onClick={toggleForm}>Finished adding books</button>
+      </Show>
     </div>
   );
 }
